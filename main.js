a641f86f2b3b40a65adac53556d0285be021a71d
Altius2010 <72147694+Altius2010@users.noreply.github.com>
2021-03-09 20:33:37 +0530

    Update main.js

--- main.js
+++ main.js
@@ -6,10 +6,10 @@ function preload() {
 }
 
 function setup() {
-    video = createCapture(VIDEO);
-    video.size(550, 500);
     canvas = createCanvas(550, 530);
     canvas.center();
+    video = createCapture(VIDEO);
+    video.size(550, 500);
     poseNet = ml5.poseNet(video, modelLoaded);
     poseNet.on('pose', gotPoses);
 }
